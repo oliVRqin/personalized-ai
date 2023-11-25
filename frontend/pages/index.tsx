f4636da23ef24183df8660fa83e78c6259cdc641
Oliver <oliver.qin1@gmail.com>
2023-11-24 19:17:01 -0800

    got WebSockets working

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,47 @@
+import { useEffect, useState, useRef } from 'react'
 
 export default function Home() {
+  const socketRef = useRef<WebSocket | null>(null)
+  const [isPaused, setIsPaused] = useState(false);
+
+  useEffect(() => {
+    socketRef.current = new WebSocket(`${process.env.NEXT_PUBLIC_WEBSOCKET_ENDPOINT}`);
+    console.log("WebSocket created: ", socketRef.current);
+  
+    socketRef.current.onopen = () => {
+      console.log("WebSocket open");
+      if (socketRef.current?.readyState === WebSocket.OPEN) {
+        socketRef.current.send("Hello from onopen");
+      }
+    };
+  
+    socketRef.current.onmessage = (event) => {
+      console.log("WebSocket message received:", event);
+      if (!isPaused && socketRef.current?.readyState === WebSocket.OPEN) {
+        const message = event.data;
+        console.log("e", message);
+        socketRef.current.send("Hello from onmessage");
+      }
+    };
+  
+    socketRef.current.onerror = (error) => {
+      console.error("WebSocket Error: ", error);
+    };
+  
+    return () => {
+      console.log("WebSocket returned");
+      if (socketRef.current?.readyState === WebSocket.OPEN) { 
+        socketRef.current.close();
+      } 
+    };
+  }, [isPaused]);
+  
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
-      Personalized AI
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
+      Title
+      <button onClick={() => setIsPaused(!isPaused)}>
+          {isPaused ? "Resume" : "Pause"}
+      </button>
     </main>
-  )
+  );
 }
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const WebSocket = require('ws');
 
 const app = express();
 
-
 dotenv.config();
+
+const server = new WebSocket.Server({ port: process.env.WS_PORT });
+
+server.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log(message);
+        ws.send('Hello! Message received!');
+    });
+    ws.on('close', () => {
+       console.log("WS closing!")
+    })
+  });
 
 app.use(cors());
 app.use(express.json());
