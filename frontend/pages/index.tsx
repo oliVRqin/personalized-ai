3dba298f9a9e1e39a8c7dd12107e58cf96b2d5c6
Oliver <oliver.qin1@gmail.com>
2023-11-26 20:49:25 -0800

    get audio to be captured on stop of recording

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,18 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
   const socketRef = useRef<WebSocket | null>(null)
-  const [isPaused, setIsPaused] = useState(false);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const audioChunksRef = useRef([]);
 
-  const configuration = { iceServers: [{ urls: /* process.env.NEXT_PUBLIC_ICE_SERVER_URL */"stun:stun.l.google.com:19302" }] };
-  let peerConnection: RTCPeerConnection;
+  const configuration = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
+  let peerConnection: any = null;
 
   const constraints = {
     audio: true,
-    video: false // Add video later
+    video: false
   };
 
   const startWebRTC = async () => {
@@ -21,11 +23,6 @@ export default function Home() {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
       console.log("stream", stream)
 
-      // Play back the obtained stream (to hear your own input)
-      const audio = new Audio();
-      console.log("audio", audio)
-      audio.srcObject = stream;
-      audio.play();
       stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
       peerConnection.onnegotiationneeded = async () => {
         try {
@@ -36,17 +33,17 @@ export default function Home() {
           console.error("Error creating offer: ", error);
         }
       };
-      peerConnection.onicecandidate = ({ candidate }) => {
+      peerConnection.onicecandidate = ({ candidate }: any) => {
         if (candidate) {
           console.log("candidate", candidate)
           socketRef.current?.send(JSON.stringify({ ice: candidate }));
         }
       };
-      peerConnection.ontrack = ({ streams: [stream] }) => {
+      peerConnection.ontrack = ({ streams: [stream] }: any) => {
         console.log("stream ontrack", stream)
-        const audio = new Audio();
+        /* const audio = new Audio();
         audio.srcObject = stream;
-        audio.play();
+        audio.play(); */
       };
     } catch (error) {
       console.error("Error creating peer connection: ", error);
@@ -54,97 +51,58 @@ export default function Home() {
   }
 
   useEffect(() => {
-    socketRef.current = new WebSocket(`${process.env.NEXT_PUBLIC_WEBSOCKET_ENDPOINT}`);
-    console.log("WebSocket created: ", socketRef.current);
-  
-    socketRef.current.onopen = () => {
-      console.log("WebSocket open");
+    if (!isPaused) {
+      socketRef.current = new WebSocket(`${process.env.NEXT_PUBLIC_WEBSOCKET_ENDPOINT}`);
+      startWebRTC();
+      // Additional logic for unpausing, if needed
+    } else {
       if (socketRef.current?.readyState === WebSocket.OPEN) {
-        console.log("Hello from onopen");
-        startWebRTC();
-      }
-    };
-  
-    /* socketRef.current.onmessage = (event) => {
-      console.log("WebSocket message received:", event);
-      if (!isPaused && socketRef.current?.readyState === WebSocket.OPEN) {
-        const message = event.data;
-        console.log("e", message);
-        socketRef.current.send("Hello from onmessage");
-      }
-    }; */
-  
-    socketRef.current.onerror = (error) => {
-      console.error("WebSocket Error: ", error);
-    };
-  
-    return () => {
-      console.log("WebSocket returned");
-      if (socketRef.current?.readyState === WebSocket.OPEN) { 
         socketRef.current.close();
-      } 
-    };
-  }, [isPaused]);
-
-  let audioChunks: any = [];
-
-  /* useEffect(() => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-          console.log("stream", stream)
-          mediaRecorderRef.current = new MediaRecorder(stream);
-          console.log("mediaRecorderRef.current", mediaRecorderRef.current)
-          mediaRecorderRef.current.ondataavailable = (event: any) => {
-            console.log("ondataavailable triggered: ", event)
-            audioChunks.push(event.data);
-          };
-          mediaRecorderRef.current.onstop = () => {
-              const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
-              const audioBuffer = new FileReader();
-              
-              audioBuffer.onload = function(event) {
-                const audioData = event.target?.result as ArrayBuffer;
-                socketRef.current?.send(audioData);
-              };
-              audioBuffer.readAsArrayBuffer(audioBlob);
-          };
-          console.log("stream.getTracks()", stream.getTracks())
-          peerConnection.addTrack(stream.getTracks()[0], stream);
-      });
-    console.log("updated audioChunks", audioChunks)
-  }, [isPaused]); */
-
-  const handleConversation = async () => {
-    if (socketRef.current?.readyState === WebSocket.OPEN) {
-      socketRef.current.send("Hello from handleConversation");
-      try {
-        peerConnection = new RTCPeerConnection(configuration);
-        console.log("peerConnection", peerConnection)
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        
-      } catch (error) {
-        console.error("Error creating peer connection: ", error);
       }
-      
+      if (peerConnection) {
+        peerConnection.close();
+      }
+      // Additional logic for pausing, if needed
     }
-    setIsPaused(!isPaused);
-  }
+  }, [isPaused, peerConnection]);
 
-  /* function startRecording() {
-    audioChunks = [];
-    mediaRecorder.start();
-  }
+  useEffect(() => {
+    if (isRecording) {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+          mediaRecorderRef.current = new MediaRecorder(stream);
+          mediaRecorderRef.current.ondataavailable = (event) => {
+            audioChunksRef.current.push(event.data);
+          };
+          mediaRecorderRef.current.start();
+        });
+    } else if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current.onstop = () => {
+        const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/mp3' });
+        const audioUrl = URL.createObjectURL(audioBlob);
+        console.log('Audio File:', audioUrl);
+        const audio = new Audio(audioUrl);
+        audio.play();
+        audioChunksRef.current = [];
+      };
+    }
+  }, [isRecording]);
 
-  function stopRecording() {
-      mediaRecorder.stop();
-  } */
-  
+  const toggleRecording = () => {
+    setIsRecording(!isRecording);
+  };
+
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-      Title
-      <button onClick={handleConversation}>
-          {isPaused ? "Resume" : "Pause"}
+      <div>Personalized AI</div>
+      <button onClick={() => setIsPaused(!isPaused)}>
+        {isPaused ? "Resume Connection" : "Pause Connection"}
+      </button>
+      <button onClick={toggleRecording}>
+        {isRecording ? "Stop Recording" : "Start Recording"}
       </button>
     </main>
   );
 }
+
